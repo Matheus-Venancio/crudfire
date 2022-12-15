c779e1c9fd5dcf0068c3a642e2cb975ccd6605ff
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-15 15:04:15 -0300

    create user and delete user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 
 import { useState, useEffect } from 'react';
 import { db } from './firebase-config';
-import{collection, doc, getDocs} from "firebase/firestore"
+import{collection, addDoc, getDocs, updateDoc, doc, deleteDoc} from "firebase/firestore"
 import './App.css';
 
+
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+
   const [users,setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users")//pegando a coleção
+
+  //Adicionando dados 
+  const createUser = async () => {
+    //Adicionando outro
+    //Adicionando o inputy de dom e idade no banco de dados
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser= async(id,age)=>{
+    const userDoc=doc(db,"users",id)
+    const newFields = {age: age + 1}
+    await updateDoc(userDoc,newFields)
+  }
+
+  const deletUser= async(id)=>{
+    const userDoc=doc(db,"users",id)
+    await deleteDoc(userDoc)
+  }
+
 
   useEffect(()=>{
     const getUsers = async () =>{
       const data = await getDocs(usersCollectionRef);
-      //Setando os dados dos usuarios
-      setUsers(data.docs.map((doc)=> ({...doc=data(), id: doc.id})))
-    }
+      //Setando os dados dos usuarios    //Doc.data estou puxando os dados
+      setUsers(data.docs.map((doc)=> ({...doc.data(), id: doc.id})));
+    };
 
-    getUsers()
-  },[])
+    getUsers();
+  },[]);
+
   return (
     <div className="App">
+      <input placeholder='name' onChange={(event) =>{setNewName(event.target.value)}}></input>
+      <input placeholder='age' onChange={(event) =>{setNewAge(event.target.value)}}></input>
+
+      <button onClick={createUser}> Create User</button>
       {users.map((user) =>{
         //Adicionando na tela
         return( 
@@ -26,6 +54,8 @@ function App() {
           {" "}
           <h1>Name: {user.name}</h1>
           <h1>Age: {user.age}</h1>
+          <button onClick={() =>{updateUser(user.id, user.age)}}>Update</button>
+          <button onClick={() =>{deletUser(user.id)}}>delete</button>
         </div>
         );
       })}
